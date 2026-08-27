da739d8aed3b4db6ddd2ec2d4f6ed92bfeae52f8
niuzigeniuzida <qq1109642497@gmail.com>
2026-08-28 00:49:48 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 860,
+    minHeight: 560,
+    backgroundColor: '#E7E2D4',
+    autoHideMenuBar: true,
+    title: '卡片笔记',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
